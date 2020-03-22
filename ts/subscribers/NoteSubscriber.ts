d7e395ac52e2d549057dd4a6196dd05a2c5a200c
Eric Rabil <ericjrabil@gmail.com>
2020-03-22 01:14:46 -0400

    Added cluster events

--- ts/subscribers/NoteSubscriber.ts
+++ ts/subscribers/NoteSubscriber.ts
@@ -1,5 +1,6 @@
-import { EventSubscriber, EntitySubscriberInterface, UpdateEvent } from "typeorm";
+import { EventSubscriber, EntitySubscriberInterface, UpdateEvent, InsertEvent, RemoveEvent } from "typeorm";
 import { Note } from "../entity/Note";
+import { SocketSupervisor } from "../sockets/SocketSupervisor";
 
 @EventSubscriber()
 export class NoteSubscriber implements EntitySubscriberInterface<Note> {
@@ -18,5 +19,33 @@ export class NoteSubscriber implements EntitySubscriberInterface<Note> {
         }
       });
     }
+  }
+
+  async afterInsert({ entity }: InsertEvent<Note>) {
+    const cluster = entity.author.cluster;
+    if (!cluster) return;
+    cluster.send({
+      action: "/note/create",
+      data: entity.json
+    });
+  }
+
+  async beforeRemove({ entity }: RemoveEvent<Note>) {
+    if (!entity) return;
+    Object.defineProperty(entity, '__id__', {
+      value: entity.id,
+      writable: false,
+      enumerable: false
+    });
+  }
+
+  async afterRemove({ entity }: RemoveEvent<Note & {__id__: string}>) {
+    if (!entity) return;
+    const cluster = SocketSupervisor.sharedInstance().userClusters[entity.authorId];
+    if (!cluster) return;
+    cluster.send({
+      action: "/note/delete",
+      data: entity['__id__']
+    });
   }
 }
